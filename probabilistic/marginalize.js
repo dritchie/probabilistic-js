f94b7a2f17e6d144bfa0714e8fc5a85788c1486f
Noah Goodman <ngoodman@stanford.edu>
2013-09-26 09:44:22 -0700

    updating marginalize

--- probabilistic/marginalize.js
+++ probabilistic/marginalize.js
@@ -15,9 +15,11 @@ function MarginalRandomPrimitive(fn, samplingFn, samplingFnArgs) {
 }
 
 //why create an object to assign the primitive? instead of just assigning the primitive?
-MarginalRandomPrimitive.prototype = Object.create(erp.RandomPrimitive.prototype)
+MarginalRandomPrimitive.prototype = new erp.RandomPrimitive //Object.create(erp.RandomPrimitive.prototype)
 
-MarginalRandomPrimitive.prototype.clearCache = function clearCache() { this.cache = {} }
+MarginalRandomPrimitive.prototype.clearCache = function clearCache() {
+    this.cache = {}
+}
 
 //this is going to work ok, because traceUpdate is written properly to that it sets asside current trace state and reinstates it when done, hence nesting will do the right thing...
 MarginalRandomPrimitive.prototype.getDist = function getDist(args) {
@@ -26,11 +28,12 @@ MarginalRandomPrimitive.prototype.getDist = function getDist(args) {
     
     if(!(stringedargs in this.cache)) {
         
-        //console.log("Generating marginal for args " +stringedargs)
+//        console.log("Generating marginal for args " +stringedargs)
         
         var dist = {}
         var fn = this.fn
-        var computation = trace.prob( function computation(){return fn.apply(this, args)} )
+        //FIXME: check that the computation runs with same address in all locations
+        var computation = function computation(){return fn.apply(this, args)}
         var samps = this.samplingFn.apply(this, [computation].concat(this.samplingFnArgs))
         //var samps = inference.traceMH(computation, 100, 1) //TODO: which inference fn..? may want rejection or enumeration sometimes.
         
@@ -84,27 +87,27 @@ marginalize = function marginalize(fn, samplingFn)
     
     var marginalInt = new MarginalRandomPrimitive(fn, samplingFn, samplingFnArgs)
     
-    var marginal = trace.prob(function marginal()//variadic..
+    var marginal = function marginal()//variadic..
                               {
                               var args = Array.prototype.slice.apply(arguments)
                               return marginalInt.sample(args)
-                              })
+                              }
     
     //this lets you set a conditioned value for the marginal ERP, even though it's variadic:
-    marginal.conditionTo = trace.prob(function conditionTo(conditionedValue){
-                                      return trace.prob(function(){
+    marginal.conditionTo = function conditionTo(conditionedValue){
+                                      return function(){
                                                  var args = Array.prototype.slice.apply(arguments)
                                                  return marginalInt.sample(args,undefined,conditionedValue)
-                                                 })
-                                      })
+                                                 }
+                                      }
     
-    marginal.clearCache = marginalInt.clearCache
+    marginal.clearCache = function() {marginalInt.clearCache()}
     
-    marginal.logprob = trace.prob(function logprob(){ //variadic
+    marginal.logprob = function logprob(){ //variadic
                                   var args = Array.prototype.slice.apply(arguments)
                                   var val = args.pop()
                                   return marginalInt.logprob(val,args)
-                                  })
+                                  }
     
     return marginal
 }
