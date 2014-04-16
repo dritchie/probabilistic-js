429d9a787937381d2463caec176a0f114a4282bd
Julius Cheng <juliusncc@gmail.com>
2014-04-16 13:36:03 -0700

    Disable nextEnum change till we figure out how to fit that in with less-dumb init.

--- probabilistic/trace.js
+++ probabilistic/trace.js
@@ -357,9 +357,10 @@ RandomExecutionTrace.prototype.nextEnumState = function nextEnumState() {
                 v.val = newval
             }
             v.logprob = v.erp.logprob(v.val, v.params)
-        } else {
-        	throw new Error("Cannot enumerate on a continuous-valed ERP")
-        }
+        } 
+       	// else {
+        // 	throw new Error("Cannot enumerate on a continuous-valed ERP")
+        // }
     }
     this.traceUpdate()
     this.enumerate=false
